4cc9b5eceb47183f502308f59f86ae93fb9872b3
shiyabin <shiyabin114@126.com>
2017-02-16 13:54:06 +0800

    zhuce

--- app/src/main/java/com/bwie/newstitleshiyabin/fragment/Fragment_WD.java
+++ app/src/main/java/com/bwie/newstitleshiyabin/fragment/Fragment_WD.java
@@ -6,8 +6,14 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.bwie.newstitleshiyabin.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 1. 类的用途
@@ -17,11 +23,47 @@ import com.bwie.newstitleshiyabin.R;
 
 
 public class Fragment_WD extends Fragment {
+
+    private View view;
+    private ListView wd_lv;
+    private  String[] textLv={"消息通知","头条商城","京东提供","我也爆料","用户反馈","系统设置"};
+
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-            View view=inflater.inflate(R.layout.fragment_wd,null);
+        view = inflater.inflate(R.layout.fragment_wd,null);
+
+        wd_lv = (ListView) view.findViewById(R.id.wd_lv);
+        wd_lv.setAdapter( new Myadapter());
 
         return view;
     }
+    class  Myadapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return textLv.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            View v=LayoutInflater.from(getActivity()).inflate(R.layout.wd_inte,null);
+            TextView wd_tv= (TextView) v.findViewById(R.id.wd_tv);
+            wd_tv.setText(textLv[position]);
+
+
+            return v;
+        }
+    };
 }
